b28f5c0b1a900c0b7bf39b76348cbab85f9f51bf
2017-CS-315 <57999221+2017-CS-315@users.noreply.github.com>
2023-07-21 00:16:43 +0500

    Starter Added

---
+++ starter/controllers/tourController.js
@@ -0,0 +1,101 @@
+
+const Tour= require('../models/tourModel');
+
+
+
+
+exports.getAllTours= async (req,res)=>{
+    //res.status(200).send('Hello from the backend')
+    try { const allTours= await Tour.find()
+        res.status(200).json({'status':'success', 'data':{'tours':allTours}})
+    }
+    catch(err){
+        res.status(404).json({
+            'status':'fail',
+            'error':err.message
+        })
+    }
+       
+    
+    }
+
+//TypeError: Cannot read properties of undefined (reading 'id') at getTour
+
+
+
+exports.getTour= async (req,res)=>{
+    /*  var id =req.params.id*1;
+    var i=0;
+    while(i<tours.length){
+        if(id==tours[i].id){
+            var tour=tours[i]
+            console.log(tour)
+            break;
+        }
+        i++
+    }*/
+   try {
+    const tour= await Tour.find({"_id":req.params.id})
+res.status(200).json({
+    'status':'success',
+    'data':tour
+})
+}
+
+catch (err){
+    res.status(404).json({
+        'status':'fail',
+        'error':err.message
+    })
+}
+    }
+
+
+exports.createNewtour= async (req,res)=>{
+  /*  var newTour=req.body;
+    newTour.id=tours[tours.length-1].id+1;
+   
+    
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/../dev-data/data/tours-simple.json`,JSON.stringify(tours),'utf-8',(err)=>{
+        if(err) {console.log(err)}*/
+
+        try {
+            const newTour= await Tour.create(req.body)
+            res.status(201).json({'status':'success', 'data':newTour})
+        }
+       catch (err){
+        res.status(400).json({
+            'status':'fail',
+            'error':err.message
+        })
+       }
+    }
+
+
+exports.changeTour= async (req,res)=>{
+    
+try {
+    const changedTour= await Tour.findByIdAndUpdate(req.params.id,req.body,{new:true, runValidators:true})
+   //const changedTour= await Tour.updateOne({"_id":req.params.id},req.body,{new:true}) 
+   res.status(200).json({'status':'success', 'data':changedTour})
+}
+
+catch(err){
+    res.status(400).json({
+        'status':'fail',
+        'error':err.message
+    })
+}  
+    }
+
+
+
+exports.deleteTour=async (req,res)=>{
+   
+    try {}
+    catch {}
+const deletedTour= await Tour.deleteOne({"_id":req.body.id});
+
+    }
+
---
+++ starter/controllers/userController.js
@@ -0,0 +1,33 @@
+
+
+exports.getAllUsers=(req,res)=>{
+    res.status(500).json({
+        'status':'error',
+        'message':'This route has not yet been implemented'
+    })
+}
+
+exports.createNewUser=(req,res)=>{
+    res.status(500).json({
+        'status':'error',
+        'message':'This route has not yet been implemented'
+    })
+}
+exports.getUser=(req,res)=>{
+    res.status(500).json({
+        'status':'error',
+        'message':'This route has not yet been implemented'
+    })
+}
+exports.changeUser=(req,res)=>{
+    res.status(500).json({
+        'status':'error',
+        'message':'This route has not yet been implemented'
+    })
+}
+exports.deleteUser=(req,res)=>{
+    res.status(500).json({
+        'status':'error',
+        'message':'This route has not yet been implemented'
+    })
+}
